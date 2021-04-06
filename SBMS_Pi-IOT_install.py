16358a1f76f222b00bcea6ad0b137c4882a6f454
Burtond <hockeydjb@hotmail.com>
2021-04-05 23:54:11 -0400

    Create SBMS_Pi-IOT_install.py
    This script assumes that IOT stack has already been installed with Grafana and InfluxDB already up and running.  IOT Stack found here: https://github.com/SensorsIot/IOTstack

---
+++ SBMS_Pi-IOT_install.py
@@ -0,0 +1,32 @@
+import os
+import socket
+
+os.system("sudo apt update")
+os.system("sudo apt upgrade -y")
+
+# Configure InfluxDB
+os.system("sudo apt install influxdb-client")
+os.system("influx -execute 'CREATE DATABASE SBMS'")
+
+os.system("sudo apt-get -y install python-influxdb")
+os.system("sudo apt-get install python-serial")
+
+# Download SBMS Serial Data collector script
+os.system("sudo wget --no-check-certificate --content-disposition 'https://raw.githubusercontent.com/Burtond/Electrodacus/master/sbms0-SerialToInfluxDB.py' -P '/home/SBMS0/'")
+os.system("sudo wget --no-check-certificate --content-disposition 'https://raw.githubusercontent.com/Burtond/Electrodacus/master/SBMS-Logger.service' -P '/etc/systemd/system/'")
+
+# Create a service to run collector script
+os.system("sudo systemctl enable SBMS-Logger.service")
+os.system("sudo systemctl start SBMS-Logger.service")
+
+# Getting data to output to screen upon completion
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+ipaddress = get_ip_address()
+os.system("curl --user admin:admin \"http://" + ipaddress + ":3000/api/datasources\" -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{\"name\":\"SBMS_Data\",\"isDefault\":true ,\"type\":\"influxdb\",\"url\":\"http://localhost:8086\",\"database\":\"SBMS\",\"access\":\"proxy\",\"basicAuth\":false}'")
+
+print "Login to http://" + ipaddress + ":3000"
+print "Username = admin, password = admin"
